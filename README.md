2cc222690d3a4319d2c748abfd7205222d7732ea
Miguel Triviño <128015870+Migu3lph@users.noreply.github.com>
2023-06-09 21:26:20 -0500

    inicial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Alura-ONE_challenge-encriptador
+challengeonecodificador5
+
+Este proyecto es parte de un curso dictado por Alura y Oracle Next Education
+
+"Cómo funciona:
+El encriptador es un trabajo complejo que permite generar una versión encriptada de un texto dado. Esta versión encriptada puede ser copiada y utilizada posteriormente. Sin embargo, es importante destacar que el encriptador no ofrece seguridad para contraseñas ni privacidad.
+
+El proyecto se desarrolló en dos modos: light y dark. El modo light presenta una interfaz clara y brillante, mientras que el modo dark ofrece una apariencia más oscura y elegante. Además, se incluye un icono animado del sol y la luna, diseñados en estilo pixel art, para representar los modos light y dark respectivamente.
+
+![presentacion](https://github.com/Migu3lph/Alura-ONE_challenge-encriptador-main/assets/128015870/f5c31ca2-cafb-4c04-a8c4-a5cb8c812fb1)
+
+Puedes ingresar cualquier texto en el campo de entrada para obtener su versión encriptada. También puedes pegar un texto previamente encriptado para desencriptarlo y volverlo a su estado original.
+
+Es importante tener en cuenta que el encriptador tiene sus limitaciones y no se recomienda utilizarlo para proteger información confidencial. Si buscas seguridad y privacidad en tus comunicaciones, es recomendable utilizar métodos de encriptación más robustos y confiables."
+
+-------------------------------------------------------------------------
+
+This project is part of a course given by Alura and Oracle Next Education
+
+"How it works:
+The encryptor is a complex project that generates an encrypted version of a given text. This encrypted version can be copied and used later. However, it is important to note that the encryptor does not provide security for passwords or privacy.
+
+The project was developed in two modes: light and dark. The light mode features a clear and bright interface, while the dark mode offers a darker and more elegant appearance. Additionally, an animated icon of the sun and the moon, designed in pixel art style, is included to represent the light and dark modes respectively.
+
+![presentacion](https://github.com/Migu3lph/Alura-ONE_challenge-encriptador-main/assets/128015870/f5c31ca2-cafb-4c04-a8c4-a5cb8c812fb1)
+
+You can enter any text in the input field to obtain its encrypted version. You can also paste a previously encrypted text to decrypt it and restore it to its original state.
+
+It is important to consider that the encryptor has its limitations and is not recommended for protecting sensitive information. If you are looking for security and privacy in your communications, it is advisable to use more robust and reliable encryption methods."
---
+++ functions.js
@@ -0,0 +1,120 @@
+const input = document.getElementById("input-text");
+const output = document.getElementById("output-text");
+const button = document.querySelectorAll("button");
+
+function onlyContainsLowercase(str) {
+    return /^[a-z0-9/ /ñ:;,.¿?¡!]+$/.test(str);
+}
+
+function encrypt() {
+    const chars = {
+    "a": "ai",
+    "e": "enter",
+    "i": "imes",
+    "o": "ober",
+    "u": "ufat"        
+    }
+
+    if (onlyContainsLowercase(input.value)) {
+        let encriptedText = input.value.replace(/[aeiou]/g, c => chars[c]);
+        document.getElementById("not-found").style.display = "none";
+        document.getElementById("result").style.display = "block";
+        output.textContent = encriptedText;
+    } else {
+        alert("Recuerda, sólo letras minúsculas y sin acentos");
+    }
+
+    document.getElementById("paste").style.display = "none";
+}
+
+function encryptDark() {
+    const chars = {
+    "m": "0",
+    "u": "1",
+    "r": "2",
+    "c": "3",
+    "i": "4",
+    "e": "5",
+    "l": "6",
+    "a": "7",
+    "g": "8",
+    "o": "9"
+    }
+
+    if (onlyContainsLowercase(input.value)) {
+        let encriptedText = input.value.replace(/[Dark]/g, c => chars[c]);
+        document.getElementById("not-found").style.display = "none";
+        document.getElementById("result").style.display = "block";
+        output.textContent = encriptedText;
+    } else {
+        alert("Recuerda, sólo letras minúsculas y sin acentos");
+    }
+
+    document.getElementById("paste").style.display = "none";
+}
+
+function desencrypt() {
+    const chars = {
+        "ai": "a",
+        "enter": "e",
+        "imes": "i",
+        "ober": "o",
+        "ufat": "u"
+        }
+
+    if (onlyContainsLowercase(input.value)) {
+        let desencriptedText = input.value.replace(/ai|enter|imes|ober|ufat/g, c => chars[c]);
+        document.getElementById("not-found").style.display = "none";
+        document.getElementById("result").style.display = "block";
+        output.textContent = desencriptedText;
+    } else {
+        alert("Recuerda, sólo letras minúsculas y sin acentos");
+    }
+
+    document.getElementById("paste").style.display = "none";
+}
+
+function desencryptDark() {
+    const chars = {
+        "0": "m",
+        "1": "u",
+        "2": "r",
+        "3": "c",
+        "4": "i",
+        "5": "e",
+        "6": "l",
+        "7": "a",
+        "8": "g",
+        "9": "o"
+        }
+
+    if (onlyContainsLowercase(input.value)) {
+        let desencriptedText = input.value.replace(/[0-9]/g, c => chars[c]);
+        document.getElementById("not-found").style.display = "none";
+        document.getElementById("result").style.display = "block";
+        output.textContent = desencriptedText;
+    } else {
+        alert("Recuerda, sólo letras minúsculas y sin acentos");
+    }
+
+    document.getElementById("paste").style.display = "none";
+}
+
+function copy() {
+    let copyText = output.innerText;
+    navigator.clipboard.writeText(copyText);
+    document.getElementById("paste").style.display = "block";
+}
+
+function paste() {
+    navigator.clipboard
+        .readText()
+        .then((clipText) => (input.value = clipText));
+}
+
+button.forEach(element => element.addEventListener("click", (event) => {
+    element.classList.add("pulse");
+    setTimeout(function() {
+        element.classList.remove("pulse");
+    }, 800);
+}));
